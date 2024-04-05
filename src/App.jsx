19c0aa69c733f0b6e9e216ce4d5e9a350f9e43ca
Abdul-Rasith <mscrasith@gmail.com>
2024-04-05 09:22:52 +0530

    first demo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Greet />
+    </>
+  );
+}
+function Greet() {
+  return (
+    <div>
+      <h1>welcome to react</h1>
+    </div>
+  );
+}
+
+export default App;
